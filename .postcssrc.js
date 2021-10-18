4ca1de814d31f5e455711e58ab25474577af938c
JUNICHI IMAI <dev@mgnstudio.com>
2021-10-18 21:54:02 +0900

    remove postcss-selector-not

--- .postcssrc.js
+++ .postcssrc.js
@@ -8,14 +8,7 @@ const autoprefixer = require('autoprefixer');
 const postcssSortMediaQueries = require('postcss-sort-media-queries');
 const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
 const postcssObjectFitImages = require('postcss-object-fit-images');
-const postcssSelectorNot = require('postcss-selector-not').default;
 
 module.exports = {
-  plugins: [
-    postcssSortMediaQueries(),
-    postcssFlexbugsFixes(),
-    postcssObjectFitImages(),
-    postcssSelectorNot(),
-    autoprefixer(),
-  ],
+  plugins: [postcssSortMediaQueries(), postcssFlexbugsFixes(), postcssObjectFitImages(), autoprefixer()],
 };
