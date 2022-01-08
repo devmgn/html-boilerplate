2162e7889c76315ec0d74e63fef6da72c66b1720
JUNICHI IMAI <dev@mgnstudio.com>
2022-01-08 23:32:35 +0900

    remove support for IE

--- .postcssrc.js
+++ .postcssrc.js
@@ -7,8 +7,7 @@
 const autoprefixer = require('autoprefixer');
 const postcssSortMediaQueries = require('postcss-sort-media-queries');
 const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
-const postcssObjectFitImages = require('postcss-object-fit-images');
 
 module.exports = {
-  plugins: [postcssSortMediaQueries(), postcssFlexbugsFixes(), postcssObjectFitImages(), autoprefixer()],
+  plugins: [postcssSortMediaQueries(), postcssFlexbugsFixes(), autoprefixer()],
 };
